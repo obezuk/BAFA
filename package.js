82b3b6144f1a08745e8deac70805a9861b9befa6
Timothy Obezuk <tobezuk@gmail.com>
2013-06-15 18:54:34 +1000

    updated font awesome and bootstrap

--- package.js
+++ package.js
@@ -5,14 +5,14 @@ Package.describe({
 Package.on_use(function(api){
 	api.use('jquery', 'client');
 	api.add_files([
-		'lib/bootstrap-2.2.2/bootstrap.min.css',
-		'lib/bootstrap-2.2.2/bootstrap.min.js',
-		'lib/font-awesome-3.0.2/font/fontawesome-webfont.eot',
-		'lib/font-awesome-3.0.2/font/FontAwesome.otf',
-		'lib/font-awesome-3.0.2/font/fontawesome-webfont.ttf',
-		'lib/font-awesome-3.0.2/font/fontawesome-webfont.woff',
-		'lib/font-awesome-3.0.2/font/fontawesome-webfont.svg',
-		'lib/font-awesome-3.0.2/css/font-awesome.css',
+		'lib/bootstrap-2.3.2/bootstrap.min.css',
+		'lib/bootstrap-2.3.2/bootstrap.min.js',
+		'lib/font-awesome-3.2.0/font/fontawesome-webfont.eot',
+		'lib/font-awesome-3.2.0/font/FontAwesome.otf',
+		'lib/font-awesome-3.2.0/font/fontawesome-webfont.ttf',
+		'lib/font-awesome-3.2.0/font/fontawesome-webfont.woff',
+		'lib/font-awesome-3.2.0/font/fontawesome-webfont.svg',
+		'lib/font-awesome-3.2.0/css/font-awesome.css',
 		'lib/animate.css/animate.min.css'
 	], 'client');
 });
